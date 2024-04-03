2ac0a204548d13d2d141afbbfccc565e1e867d40
Aklowa <aklowadrumz@gmail.com>
2024-04-03 20:15:03 +0000

    changed the bg-color of the footer

--- components/foot/footer.vue
+++ components/foot/footer.vue
@@ -1,6 +1,6 @@
 <template>
-	<footer class="px-4 divide-y dark:bg-gray-100 dark:text-gray-800">
-		<div class="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
+	<footer class="px-4 divide-y dark:bg-blue-100 dark:text-gray-800">
+		<div class="container flex flex-col justify-between py-1 mx-auto space-y-8 lg:flex-row lg:space-y-0">
 			<div class="lg:w-1/3">
 				<a rel="noopener noreferrer" href="#" class="flex justify-center space-x-3 lg:justify-start">
 					<div class="flex items-center justify-center w-25 h-25 rounded-full ">
@@ -74,6 +74,6 @@
 				</div>
 			</div>
 		</div>
-		<div class="py-6 text-sm text-center dark:text-gray-600">© 2024 Aklowa Drum Band. All rights reserved.</div>
+		<div class="py-3 text-sm text-center ">© 2024 Aklowa Drum Band. All rights reserved.</div>
 	</footer>
 </template>
