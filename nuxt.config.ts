333b893e4e2dcb17bdfd0f1cff103f6c6bf1bfe7
Aklowa <aklowadrumz@gmail.com>
2024-04-03 23:49:16 +0000

    update the nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,28 @@
+import { Static } from "vue";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
+  ssr: false,
+  target: Static,
+  app: {
+    baseURL: '/aklowadrumband/', // baseURL: '/<repository>/'
+    buildAssetsDir: 'assets',
+    head: {
+      title: 'Aklowa Drum Band',
+      meta: [
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+        ],
+
+    }
+},
+build: {
+transpile: ['@fortawesome/vue-fontawesome'],
+chunkSizeWarningLimit: 1600,
+cssMinify: {
+  preset: 'default',
+},
+},
     
     modules: ['@hypernym/nuxt-gsap','@nuxtjs/tailwindcss', 'nuxt-purgecss' ],
   tailwindcss: {
